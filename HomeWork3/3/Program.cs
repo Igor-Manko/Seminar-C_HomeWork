23b70ac3ae31b6c66a3c4f5343e5f457d55b850a
Igor-Manko <manko161igor@gmail.com>
2022-07-03 19:24:40 +0300

    возыедение в 3 степень чисел до N

---
+++ HomeWork3/3/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// Первый вариант
+
+int cubeTable(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        //int result = i * i * i;
+        Console.Write(Math.Pow(i, 3) + " | ");
+    }
+    return n;
+}
+
+Console.Write("Введите число для расчета таблицы: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+cubeTable(number); 
+
+/* Второй вариант
+
+double cube(double n)
+{
+    double a = Math.Pow(n, 3);
+    return a;
+}
+
+Console.Write("Введите число для расчета таблицы: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+while (i <= number) 
+{
+    Console.Write(cube(i) + " | ");
+    i++;
+}
+*/
