0573b9ba476f3fe508126c8a97ccc3dd80faf354
Igor-Manko <manko161igor@gmail.com>
2022-07-03 19:04:19 +0300

    расчет расстояния в 3D между точками

--- HomeWork3/2/Program.cs
+++ HomeWork3/2/Program.cs
@@ -5,28 +5,33 @@
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
 
-double Distance(int x1, int x2, int y1, int y2, int z1, int z2)
+double points(int a, int b)
 {
-    double dis = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2) + Math.Pow((z2 - z1), 2));
+    double p = Math.Pow((b - a), 2);
+    return p;
+}
+
+double distance(int a1, int b1, int c1, int a2, int b2, int c2)
+{
+    //double dis = Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2) + Math.Pow((c2 - c1), 2));
+    double dis = Math.Sqrt(points(a1, a2) + points(b1, b2) + points(c1, c2));
     return dis;
 }
 
-
-
 Console.WriteLine("Введите координаты первой точки:");
 Console.Write("X1: ");
-int n11 = Convert.ToInt32(Console.ReadLine());
+int X1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Y1: ");
-int n12 = Convert.ToInt32(Console.ReadLine());
+int Y1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Z1: ");
-int n13 = Convert.ToInt32(Console.ReadLine());
+int Z1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите координаты второй точки:");
 Console.Write("X2: ");
-int n21 = Convert.ToInt32(Console.ReadLine());
+int X2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Y2: ");
-int n22 = Convert.ToInt32(Console.ReadLine());
+int Y2 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Z2: ");
-int n23 = Convert.ToInt32(Console.ReadLine());
+int Z2 = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine(Distance(n11, n21, n12, n22, n13, n23));
+Console.WriteLine(distance(X1, Y1, Z1, X2, Y2, Z2));
