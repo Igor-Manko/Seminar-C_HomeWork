6eaca02406bfeafc3c690517294ce1f75eef5021
Igor-Manko <manko161igor@gmail.com>
2022-07-04 23:23:31 +0300

    exponention of a number

---
+++ HomeWork4/1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+double Exponention(double a, double b)
+{
+    double result = Math.Pow(a, b);
+    return result;
+}
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень числа: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Результат возведения числа " + n + " в степень " + m + " = " + Exponention(n, m));
+
