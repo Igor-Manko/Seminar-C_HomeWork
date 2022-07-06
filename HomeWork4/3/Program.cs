5c9cfc7d919b262fe7a16d323e093ab7e55d94a5
Igor-Manko <manko161igor@gmail.com>
2022-07-06 13:31:08 +0300

    converting number to array

---
+++ HomeWork4/3/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Введете числа для преобразования в массив: ");
+string str = Console.ReadLine();
+char[] arr = str.ToCharArray();
+Console.WriteLine(str);
+
+/*
+int[] fillArray(int[] arr)
+{
+    int length = arr.Length;
+    int index = 0;
+    while (index < length)
+    {
+        arr[index] = Convert.ToInt32(Console.ReadLine());
+        index++;
+    }
+    return arr;
+}
+
+
+int[] array = new int[8];
+fillArray(array);
+
+Console.Write(String.Join(" ", array)); // печать массива сразу
+*/
