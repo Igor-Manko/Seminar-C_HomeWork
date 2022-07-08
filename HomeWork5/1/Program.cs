c8c298b57d5b75783a5fbc840390cdba7c6142e6
Igor-Manko <manko161igor@gmail.com>
2022-07-09 01:08:46 +0700

    sum random number in array

---
+++ HomeWork5/1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+// [345, 897, 568, 234] -> 2
+
+int[] fillArray(int[] arr)
+{
+    int length = arr.Length;
+    int index = 0;
+    while (index < length)
+    {
+        arr[index] = new Random().Next(100, 1000);
+        index++;
+    }
+    return arr;
+}
+
+int myEven(int[] arr)
+{
+    int count = 0;
+    int index = 0;
+    int length = arr.Length;
+
+    while (index < length)
+    {
+        if (arr[index] % 2 == 0) count++;       // проверка четности элемента массива 
+        index++;
+    }
+    return count;
+}
+
+
+int[] array = new int[4];
+fillArray(array);
+
+Console.WriteLine(String.Join(" ", array));        // функция печати массива
+Console.Write("Количество четных чисел в массиве: " + myEven(array));
