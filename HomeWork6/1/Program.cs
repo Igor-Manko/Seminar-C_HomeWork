77a2ddd4ad5a12ff1cf4470e5759b031cbece554
Igor-Manko <manko161igor@gmail.com>
2022-07-18 11:04:28 +0700

    number > 0 in Array

---
+++ HomeWork6/1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int ReadArrPositiv()
+{
+    Console.Write("Введите числа: ");
+    int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+    int count = 0;
+
+    for (int index = 0; index < arr.Length; index++)
+    {
+        if (arr[index] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+
+Console.WriteLine($"Количество элементов > 0: {ReadArrPositiv()}");
