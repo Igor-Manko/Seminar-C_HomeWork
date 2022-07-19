4093db3b95c98015c8c290d697efd017c7c394c2
Igor-Manko <manko161igor@gmail.com>
2022-07-20 01:30:00 +0700

    creat two dimensional array

---
+++ HomeWork7/1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+
+void RandomMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{matr[i, j] = new Random().Next(-9, 10)} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] matrix = new int[3, 4];
+RandomMatrix(matrix);
