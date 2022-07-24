c1f43b9460ccde2259f720286b06ab06ed00c3fd
Igor-Manko <manko161igor@gmail.com>
2022-07-24 22:02:25 +0700

    Sort Array

---
+++ HomeWork8/1/Program.cs
@@ -0,0 +1,66 @@
+﻿// // Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы 
+// каждой строки двумерного массива.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 1 2 4 7
+// 2 3 5 9
+// 2 4 4 8
+
+void FillArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            //arr[i,j] = new Random().Next(1,10);
+            Console.Write($" {arr[i, j] = new Random().Next(1, 9)}");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void MySort(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(1) - 1; k++)
+            {
+                if (arr[i, k] > arr[i, k + 1])    // смена знака для выбора возрастания или убывания сортировки
+                {
+                    int temp = arr[i, k + 1];
+                    arr[i, k + 1] = arr[i, k];
+                    arr[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void PrintSortArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] matrix = new int[4, 4];
+Console.WriteLine("Сгенерированный рандомный массив: ");
+FillArray(matrix);
+Console.WriteLine("Массив после сортировки: ");
+MySort(matrix);
+PrintSortArray(matrix);
+
+
